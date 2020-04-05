f4785807c396200b77625c849ac7cfc8a2147190
Anushetty1027 <63201803+Anushetty1027@users.noreply.github.com>
2020-04-05 15:10:24 -0400

    Add files via upload

---
+++ FINAL PROJECT/vectors.m
@@ -0,0 +1,6 @@
+% MachA vector (5 elements)
+mach=[0.0294; 0.04408; 0.05877; 0.07347; 0.08816];
+% Angle of attack alphaA vector (5 elements) [deg]
+alphad=[-10.0; -5.0;  0.0;  5.0;  10.0];
+% Angle of sideslip beta vector (8 elements) [deg]
+% beta=[-28.0; -24.0; -20.0; -16.0; -12.0; -8.0; -4.0; 0.0;  4.0;  8.0; 12.0; 16.0; 20.0; 24.0; 28.0];
